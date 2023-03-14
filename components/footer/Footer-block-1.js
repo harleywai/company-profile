55fd3ff86164b5a76ae87b88c0fe1c5e5354d765
Harley Wai <28331148+harleywai@users.noreply.github.com>
2023-03-14 17:26:43 +0630

    Footer logo change

--- components/footer/Footer-block-1.js
+++ components/footer/Footer-block-1.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const FooterBlock1 = () => {
   return (
     <div className="footer-block">
-      <div className='site-logo'><Image className="logo" src="/images/logo.svg" width={187} height={80} alt="hext" /></div>
+      <div className='site-logo'><Image className="logo" src="/images/orientwebs_logo.svg" width={187} height={80} alt="hext" /></div>
       <p>
         OrientWebs is a world class affordable web hosting provider. We provider Secure, reliable and fast web hosting with a 99.9% uptime guarantee <Link href="/about">Read more about Us</Link></p>
     </div>
